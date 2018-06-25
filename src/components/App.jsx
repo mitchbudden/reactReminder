790a48625cce69cf9a958fa11336098db82652b4
Budden, Mitch <MitchBudden@quickenloans.com>
2018-06-25 16:22:09 -0400

    Adding timestamp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
 
 class App extends Component {
     constructor(props) {
@@ -11,7 +12,7 @@ class App extends Component {
     }
 
     addReminder() {
-        this.props.addReminder(this.state.text);
+        this.props.addReminder(this.state.text, this.state.dueDate);
     }
 
     deleteReminder(id) {
@@ -26,7 +27,10 @@ class App extends Component {
                 reminders.map(reminder => {
                     return (
                         <li key={reminder.id} className="list-group-item">
-                            <div className="list-item">{reminder.text}</div>
+                            <div className="list-item">
+                                <div>{reminder.text}</div>
+                                <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                            </div>
                             <div className="list-item delete-button"
                                 onClick={() => this.deleteReminder(reminder.id)}>
                                 &#x2715;
@@ -51,6 +55,11 @@ class App extends Component {
                             className="form-control"
                             placeholder="I have to..."
                             onChange={event => this.setState({text: event.target.value})}
+                        />
+                        <input 
+                            className="form-control"
+                            type="datetime-local"
+                            onChange={event => this.setState({dueDate: event.target.value})}
                         />
                     </div>
                     <button

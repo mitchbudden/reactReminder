16d7930ad25e8288249ded77c606f851bd1f3601
Budden, Mitch <MitchBudden@quickenloans.com>
2018-06-25 15:47:04 -0400

    Styling & Delete button on reminders

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
-import { ADD_REMINDER } from '../constants/constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants/constants';
 
 export const addReminder = (text) => {
     const action = {
         type: ADD_REMINDER,
         text
     }
-    console.log('action in addReminder', action);
+    return action;
+}
+
+export const deleteReminder = (id) => {
+    const action = {
+        type: DELETE_REMINDER,
+        id
+    }
     return action;
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder } from '../actions';
+import { addReminder, deleteReminder } from '../actions';
 
 class App extends Component {
     constructor(props) {
@@ -11,17 +11,41 @@ class App extends Component {
     }
 
     addReminder() {
-        console.log('this', this);
         this.props.addReminder(this.state.text);
+    }
+
+    deleteReminder(id) {
+        this.props.deleteReminder(id);
+    }
+
+    renderReminders() {
+        const { reminders } = this.props;
+        return (
+            <ul className="list-group col-sm-4">
+            {
+                reminders.map(reminder => {
+                    return (
+                        <li key={reminder.id} className="list-group-item">
+                            <div className="list-item">{reminder.text}</div>
+                            <div className="list-item delete-button"
+                                onClick={() => this.deleteReminder(reminder.id)}>
+                                &#x2715;
+                            </div>
+                        </li>
+                    )
+                })
+            }
+            </ul>
+        )
     }
 
     render() {
         return (
             <div className="App">
-                <div>
+                <div className="title">
                     ReminderPro
                 </div>
-                <div className="form-inline">
+                <div className="form-inline reminder-form">
                     <div className="form-group">
                         <input 
                             className="form-control"
@@ -35,11 +59,18 @@ class App extends Component {
                         onClick={() => this.addReminder()}>
                         Add Reminder
                     </button>
+                    { this.renderReminders() }                    
                 </div>
             </div>
         )
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        reminders: state
+    }
+}
+
 //2nd parameter below is a shortcut for avoiding writing out MapDispatchToProps as it's own function
-export default connect(null, { addReminder })(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
